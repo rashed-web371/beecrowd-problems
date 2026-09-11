bfa135364d0ca4d42fe9708511c3f6e446418dcb
Rashed Rabea Abd Garwan <rashdrbyjrwan@gmail.com>
2026-09-11 10:27:01 +0300

    Create 1004-Simple-Product.c

---
+++ 1004-Simple-Product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+int A;
+    //printf("A:");
+        scanf("%i", &A);
+int B;
+    //printf("B:");
+        scanf("%i", &B);
+int PROD = A * B;
+
+printf("PROD = %i\n", PROD);
+
+return 0;
+}
