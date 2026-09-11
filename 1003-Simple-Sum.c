7c8703a6a41425804c88766f25ad9b9d29860fa6
Rashed Rabea Abd Garwan <rashdrbyjrwan@gmail.com>
2026-09-11 10:24:04 +0300

    Create 1003-Simple-Sum.c

---
+++ 1003-Simple-Sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+int A;
+    //printf("A:");
+        scanf("%i", &A);
+int B;
+    //printf("B:");
+        scanf("%i", &B);
+
+int SOMA = A + B;
+
+printf("SOMA = %i\n", SOMA);
+
+return 0;
+}
