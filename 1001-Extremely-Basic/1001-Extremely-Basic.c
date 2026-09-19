88988024d09f3821fb5e2bb0c77427774d12163a
Rashed Rabea Abd Garwan <rashdrbyjrwan@gmail.com>
2026-09-19 23:13:49 +0300

    Update and rename 1001-Extremely-Basic.c to 1001-Extremely-Basic/1001-Extremely-Basic.c

---
+++ 1001-Extremely-Basic/1001-Extremely-Basic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int first_number, second_number;
+    
+    scanf("%i", &first_number);
+    scanf("%i", &second_number);
+
+    int sum = first_number + second_number;
+
+    printf("X = %i\n", sum);
+    return 0;
+}
